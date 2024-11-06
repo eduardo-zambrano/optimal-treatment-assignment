30cec693aa50fd2324fc45571e3f71fa207b74dc
Eduardo <eduardo@thewayofit.com>
2024-11-06 14:01:50 -0800

    Created the table that the julia code reads

--- Figure_3_Table_3/figure-3-table-3-code.jl
+++ Figure_3_Table_3/figure-3-table-3-code.jl
@@ -53,8 +53,9 @@ function solve_for_z(eeb_u, eea_l, eeb_l, eea_u, gamma)
 end
 
 
+# Preparing Table 3
 
-# Horowitz - Manski bounds on levels (From Michelle's calculations)
+## Horowitz - Manski bounds, gamma = 0 (From Michelle's calculations)
 gamma_0 = 0
 
 eea_u_hm_level = 10.7493
@@ -65,26 +66,9 @@ eeb_l_hm_level = 6.1253
 
 sol_hm_level = solve_for_z(eeb_u_hm_level, eea_l_hm_level, eeb_l_hm_level, eea_u_hm_level, gamma_0)
 
-# Horowitz - Manski bounds, gamma = 1 (from Lee 2009, p. 1080)
-gamma = 1
 
-E_l_log_y_a = 1.520
-E_u_log_y_a = 2.332
-
-E_l_log_y_b = 1.586
-E_u_log_y_b = 2.321
-
-eea_u_hm = f_inv(E_u_log_y_a, gamma)
-eea_l_hm =f_inv(E_l_log_y_a, gamma)
-
-eeb_u_hm = f_inv(E_u_log_y_b, gamma)
-eeb_l_hm =f_inv(E_l_log_y_b, gamma)
-
-sol_hm_1 = solve_for_z(eeb_u_hm, eea_l_hm, eeb_l_hm, eea_u_hm, gamma)
-
-# Horowitz - Manski bounds, gamma = 2 (from Lee 2009, p. 1080, and Michelle's computations)
-
-gamma = 2
+## Horowitz - Manski bounds, gamma = 2 (from Lee 2009, p. 1080, and Michelle's computations)
+gamma_2 = 2
 
 E_l_f_y_a_2 = -.2457982
 E_u_f_y_a_2 = -.1106156
@@ -92,55 +76,15 @@ E_u_f_y_a_2 = -.1106156
 E_l_f_y_b_2 = -.2661934
 E_u_f_y_b_2 = -.1167666
 
-eea_u_hm_2 = f_inv(E_u_f_y_a_2, gamma)
-eea_l_hm_2 =f_inv(E_l_f_y_a_2, gamma)
+eea_u_hm_2 = f_inv(E_u_f_y_a_2, gamma_2)
+eea_l_hm_2 =f_inv(E_l_f_y_a_2, gamma_2)
 
-eeb_u_hm_2 = f_inv(E_u_f_y_b_2, gamma)
-eeb_l_hm_2 =f_inv(E_l_f_y_b_2, gamma)
+eeb_u_hm_2 = f_inv(E_u_f_y_b_2, gamma_2)
+eeb_l_hm_2 =f_inv(E_l_f_y_b_2, gamma_2)
 
-sol_hm_2 = solve_for_z(eeb_u_hm_2, eea_l_hm_2, eeb_l_hm_2, eea_u_hm_2, gamma)
+sol_hm_2 = solve_for_z(eeb_u_hm_2, eea_l_hm_2, eeb_l_hm_2, eea_u_hm_2, gamma_2)
 
-
-# Lee bounds, gamma = 1 (From Lee 2009, p. 1092)
-gamma_1 = 1
-
-E_l_log_y_a_Lee = 1.997
-E_u_log_y_a_Lee = 1.997
-
-E_l_log_y_b_Lee = 1.978
-E_u_log_y_b_Lee = 2.090
-
-eea_u_Lee = f_inv(E_u_log_y_a_Lee, gamma_1)
-eea_l_Lee =f_inv(E_l_log_y_a_Lee, gamma_1)
-
-eeb_u_Lee = f_inv(E_u_log_y_b_Lee, gamma_1)
-eeb_l_Lee =f_inv(E_l_log_y_b_Lee, gamma_1)
-
-sol_l = solve_for_z(eeb_u_Lee, eea_l_Lee, eeb_l_Lee, eea_u_Lee, gamma)
-
-# Lee bounds, gamma = 2 (From Lee 2009, p. 1092 and Michelle's computations)
-gamma_2 = 2
-
-E_l_f_y_a_Lee_2 = -.1520497
-E_u_f_y_a_Lee_2 = -.1520497
-
-E_l_f_y_b_Lee_2 = -.1538839
-E_u_f_y_b_Lee_2 = -.1290454
-
-
-eea_u_Lee_2 = f_inv(E_u_f_y_a_Lee_2, gamma_2)
-eea_l_Lee_2 =f_inv(E_l_f_y_a_Lee_2, gamma_2)
-
-eeb_u_Lee_2 = f_inv(E_u_f_y_b_Lee_2, gamma_2)
-eeb_l_Lee_2 =f_inv(E_l_f_y_b_Lee_2, gamma_2)
-
-sol_l_2 = solve_for_z(eeb_u_Lee_2, eea_l_Lee_2, eeb_l_Lee_2, eea_u_Lee_2, gamma)
-
-
-
-# Lee bounds on levels (From Michelle's calculations)
-# gamma = 0
-
+## Lee bounds, gamma = 0 (From Michelle's calculations)
 eea_u_Lee_level = 7.9156
 eea_l_Lee_level = 7.9156
 
@@ -150,24 +94,31 @@ eeb_l_Lee_level = 7.5262
 sol_l_level = solve_for_z(eeb_u_Lee_level, eea_l_Lee_level, eeb_l_Lee_level, eea_u_Lee_level, gamma_0)
 
 
-# Chen and Flores bounds (From Chen and Flores 2014, p. 533)
-gamma_1 = 1
-E_l_log_y_a_CF = 1.972
-E_u_log_y_a_CF = 1.972
+## Lee bounds, gamma = 2 (From Lee 2009, p. 1092 and Michelle's computations)
+E_l_f_y_a_Lee_2 = -.1520497
+E_u_f_y_a_Lee_2 = -.1520497
 
-E_l_log_y_b_CF = 2.027
-E_u_log_y_b_CF = 2.102
+E_l_f_y_b_Lee_2 = -.1538839
+E_u_f_y_b_Lee_2 = -.1290454
 
-eea_u_CF = f_inv(E_u_log_y_a_CF, gamma_1)
-eea_l_CF =f_inv(E_l_log_y_a_CF, gamma_1)
+eea_u_Lee_2 = f_inv(E_u_f_y_a_Lee_2, gamma_2)
+eea_l_Lee_2 =f_inv(E_l_f_y_a_Lee_2, gamma_2)
 
-eeb_u_CF = f_inv(E_u_log_y_b_CF, gamma_1)
-eeb_l_CF =f_inv(E_l_log_y_b_CF, gamma_1)
+eeb_u_Lee_2 = f_inv(E_u_f_y_b_Lee_2, gamma_2)
+eeb_l_Lee_2 =f_inv(E_l_f_y_b_Lee_2, gamma_2)
 
-sol_CF = solve_for_z(eeb_u_CF, eea_l_CF, eeb_l_CF, eea_u_CF, gamma_1)
+sol_l_2 = solve_for_z(eeb_u_Lee_2, eea_l_Lee_2, eeb_l_Lee_2, eea_u_Lee_2, gamma_2)
 
-# Chen and Flores bounds (From Michelle's calculations)
-# gamma = 2
+
+## Chen and Flores bounds, gamma = 0 (From Michelle's calculations)
+eea_u_CF_level = eea_u_Lee_level
+eea_l_CF_level = eea_l_Lee_level
+eeb_u_CF_level = eeb_u_Lee_level
+eeb_l_CF_level = 8.3317
+
+sol_CF_level = solve_for_z(eeb_u_CF_level, eea_l_CF_level, eeb_l_CF_level, eea_u_CF_level, gamma_0)
+
+## Chen and Flores bounds, gamma = 2 (From Michelle's calculations)
 E_l_f_y_a_CF_2 = E_l_f_y_a_Lee_2
 E_u_f_y_a_CF_2 = E_u_f_y_a_Lee_2
 
@@ -182,35 +133,12 @@ eeb_l_CF_2 =f_inv(E_l_f_y_b_CF_2, gamma_2)
 
 sol_CF = solve_for_z(eeb_u_CF_2, eea_l_CF_2, eeb_l_CF_2, eea_u_CF_2, gamma_2)
 
-# Chen and Flores bounds on levels (From Michelle's calculations)
-# gamma = 0
+# Preparing Figure 3
 
-eea_u_CF_level = eea_u_Lee_level
-eea_l_CF_level = eea_l_Lee_level
-eeb_u_CF_level = eeb_u_Lee_level
-eeb_l_CF_level = 8.3317
-
-sol_CF_level = solve_for_z(eeb_u_CF_level, eea_l_CF_level, eeb_l_CF_level, eea_u_CF_level, gamma_0)
-
-# Generate z values
+## Generate z values
 z_values = 0:0.01:1
 
-# Calculate LHS and RHS values for Horowitz-Manski (HM), Lee and CF bounds, gamma = 2
-# gamma = 2
-
-rhs_values_hm_2 = [RHS(eeb_u_hm_2, eea_l_hm_2, gamma_2, z) for z in z_values]
-lhs_values_hm_2 = [LHS(eeb_l_hm_2, eea_u_hm_2, gamma_2, z) for z in z_values]
-
-rhs_values_Lee_2 = [RHS(eeb_u_Lee_2, eea_l_Lee_2, gamma_2, z) for z in z_values]
-lhs_values_Lee_2 = [LHS(eeb_l_Lee_2, eea_u_Lee_2, gamma_2, z) for z in z_values]
-
-rhs_values_CF_2 = [RHS(eeb_u_CF_2, eea_l_CF_2, gamma_2, z) for z in z_values]
-lhs_values_CF_2 = [LHS(eeb_l_CF_2, eea_u_CF_2, gamma_2, z) for z in z_values]
-
-# Calculate LHS and RHS values for Horowitz-Manski (HM), Lee and CF bounds in levels
-
-# gamma_0 = 0
-
+## Calculate LHS and RHS values for Horowitz-Manski (HM), Lee and CF bounds, gamma = 0
 rhs_values_hm_level = [RHS(eeb_u_hm_level, eea_l_hm_level, gamma_0, z) for z in z_values]
 lhs_values_hm_level = [LHS(eeb_l_hm_level, eea_u_hm_level, gamma_0, z) for z in z_values]
 
@@ -220,8 +148,17 @@ lhs_values_Lee_level = [LHS(eeb_l_Lee_level, eea_u_Lee_level, gamma_0, z) for z 
 rhs_values_CF_level = [RHS(eeb_u_CF_level, eea_l_CF_level, gamma_0, z) for z in z_values]
 lhs_values_CF_level = [LHS(eeb_l_CF_level, eea_u_CF_level, gamma_0, z) for z in z_values]
 
+## Calculate LHS and RHS values for Horowitz-Manski (HM), Lee and CF bounds, gamma = 2
+rhs_values_hm_2 = [RHS(eeb_u_hm_2, eea_l_hm_2, gamma_2, z) for z in z_values]
+lhs_values_hm_2 = [LHS(eeb_l_hm_2, eea_u_hm_2, gamma_2, z) for z in z_values]
 
-# Create a figure with six subplots (axes)
+rhs_values_Lee_2 = [RHS(eeb_u_Lee_2, eea_l_Lee_2, gamma_2, z) for z in z_values]
+lhs_values_Lee_2 = [LHS(eeb_l_Lee_2, eea_u_Lee_2, gamma_2, z) for z in z_values]
+
+rhs_values_CF_2 = [RHS(eeb_u_CF_2, eea_l_CF_2, gamma_2, z) for z in z_values]
+lhs_values_CF_2 = [LHS(eeb_l_CF_2, eea_u_CF_2, gamma_2, z) for z in z_values]
+
+## Create a figure with six subplots (axes)
 fig = Figure();
 
 ax1 = Axis(fig[1, 1], ylabel="EE Regret, γ=0", title="w / Horowitz-Manski Bounds")
@@ -231,33 +168,32 @@ ax4 = Axis(fig[2, 1], ylabel="EE Regret, γ=2", xlabel="δ")
 ax5 = Axis(fig[2, 2], xlabel="δ")
 ax6 = Axis(fig[2, 3], xlabel="δ")
 
-# Plot LHS and RHS on the first axis (Horowitz-Manski bounds)
+## Plot LHS and RHS on the first axis (Horowitz-Manski bounds)
 lines!(ax4, z_values, lhs_values_hm_2, label="LHS")
 lines!(ax4, z_values, rhs_values_hm_2, label="RHS")
 
-# Plot LHS and RHS on the second axis (Lee bounds)
+## Plot LHS and RHS on the second axis (Lee bounds)
 lines!(ax5, z_values, lhs_values_Lee_2, label="LHS")
 lines!(ax5, z_values, rhs_values_Lee_2, label="RHS")
 
-# Plot LHS and RHS on the third axis (Chen-Flores bounds)
+## Plot LHS and RHS on the third axis (Chen-Flores bounds)
 lines!(ax6, z_values, lhs_values_CF_2, label="LHS")
 lines!(ax6, z_values, rhs_values_CF_2, label="RHS")
 
-# Plot LHS and RHS on the fourth axis (level Horowitz-Manski bounds)
+## Plot LHS and RHS on the fourth axis (level Horowitz-Manski bounds)
 lines!(ax1, z_values, lhs_values_hm_level, label="LHS")
 lines!(ax1, z_values, rhs_values_hm_level, label="RHS")
 
-# Plot LHS and RHS on the fifth axis (level Lee bounds)
+## Plot LHS and RHS on the fifth axis (level Lee bounds)
 lines!(ax2, z_values, lhs_values_Lee_level, label="LHS")
 lines!(ax2, z_values, rhs_values_Lee_level, label="RHS")
 
-# Plot LHS and RHS on the sixth axis (level Chen-Flores bounds)
+## Plot LHS and RHS on the sixth axis (level Chen-Flores bounds)
 lines!(ax3, z_values, lhs_values_CF_level, label="LHS")
 lines!(ax3, z_values, rhs_values_CF_level, label="RHS")
 
-# Link the y-axes of both plots
+## Link the y-axes of both plots
 linkyaxes!(ax1, ax2, ax3, ax4, ax5, ax6)
-
 
 fig
 
